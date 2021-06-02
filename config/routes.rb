7e0401a1d841ae99fc155cd6f6cd7ca1ad9961f9
halezmarie <teacherhaleymarie@gmail.com>
2021-06-01 19:04:44 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,7 @@ Rails.application.routes.draw do
   get 'about', to: 'static#about' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+# static routes will render a view that does NOT change, like an about page or contact page etc on a website
+# dynamic routes accept PARAMS and render different content based on the params. 
+# ex: blog's post page that contains a specific article. 
